14c2d37fd0b3ad31a06940ba1d64bc313eb43891
Weslley <wvictor07@gmail.com>
2023-04-14 00:02:10 +0100

    Improved code and README.me file

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ To split a pdf file into its multiple pages, run the commands
 pip3 install -r requirements
 python3 split.py <pdf_filename>
 ```
+
+The command will create multiple PDF files in the `output/` folder and rename them in the format `page_<page-number>`.
--- split.py
+++ split.py
@@ -5,10 +5,9 @@ from PyPDF2 import PdfReader, PdfWriter
 output_folder_path = os.path.join(os.getcwd(), 'output')
 
 def split_pdf(pdf_filename):    
-    file_base_name = pdf_filename.replace('.pdf', '')
     if not os.path.exists(output_folder_path):
         os.mkdir(output_folder_path)
-        
+    print(output_folder_path)
     pdf = PdfReader(pdf_filename)
     for page_num in range(len(pdf.pages)):
         pdfWriter = PdfWriter()
@@ -16,7 +15,7 @@ def split_pdf(pdf_filename):
 
         with open(
             os.path.join(output_folder_path,
-            '{0}_Page{1}.pdf'.format(file_base_name, page_num+1)),
+            '{0}/page-{1}.pdf'.format(output_folder_path, page_num+1)),
             'wb'
         ) as f:
             pdfWriter.write(f)
