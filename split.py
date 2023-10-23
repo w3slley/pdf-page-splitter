424560e38654e44efe30f9ec712cfd9a3e168275
Weslley Vieira <w3slley@pm.me>
2023-10-23 21:01:45 +0100

    Small improvements

--- split.py
+++ split.py
@@ -4,25 +4,18 @@ from PyPDF2 import PdfReader, PdfWriter
 
 output_folder_path = os.path.join(os.getcwd(), 'output')
 
-def split_pdf(pdf_filename):    
+def split_pdf(pdf_filename):
     if not os.path.exists(output_folder_path):
         os.mkdir(output_folder_path)
-    print(output_folder_path)
     pdf = PdfReader(pdf_filename)
     for page_num in range(len(pdf.pages)):
         pdfWriter = PdfWriter()
         pdfWriter.add_page(pdf.pages[page_num])
-
-        with open(
-            os.path.join(output_folder_path,
-            '{0}/page-{1}.pdf'.format(output_folder_path, page_num+1)),
-            'wb'
-        ) as f:
+        with open(os.path.join(output_folder_path, '{0}/page-{1}.pdf'.format(output_folder_path, page_num+1)), 'wb') as f:
             pdfWriter.write(f)
             f.close()
 
-
-if(len(sys.argv) != 2):
+if (len(sys.argv) < 2):
     print('Error: please add the pdf filename as an argument')
 else:
     pdf_filename = sys.argv[1]
